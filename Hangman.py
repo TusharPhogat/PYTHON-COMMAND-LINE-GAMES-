82d1c834e252d3daa01d471a64f7891535faa040
TUSHAR PHOGAT <tusharphogat999@gmail.com>
2020-09-20 16:58:39 +0530

    Fourth Commit

--- Hangman.py
+++ Hangman.py
@@ -4,19 +4,16 @@ Hangman --> How it works
 
 """
 import time
-def split(word):
-    for char in word:
-        return char
+
 def ask():
     word = "test"
     guess = []
-    guesses =[]
-    word_chars = split(word)
     turns = len(word)+3
+    guessed_word = []
     print("Do you want to continue(y/n):", end="")
     ans = input()
     if ans.lower() == "y" or ans.lower() == "yes":
-        while turns > 0 and guesses != word_chars:
+        while turns > 0 and guessed_word != word:
             if turns == len(word)+3:
                 chars = input("Enter a char of word:")
                 guess.append(chars)
@@ -32,7 +29,6 @@ def ask():
                 turns -= 1
             else:
                 print("You need to enter a single character")
-                
     else:
         print("Exiting Hangman", end="")
         for i in range(0,3):
